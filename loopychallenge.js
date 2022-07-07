992ecb724acd218669626037df8a1f075bd0a148
sachieko <kirazetsubou@gmail.com>
2022-07-06 21:27:34 -0600

    Changes to pass lint added.

--- loopychallenge.js
+++ loopychallenge.js
@@ -1,8 +1,8 @@
 for (let number = 100; number <= 200; number++) {
   // Starts running at 100, and stops inclusively at 200.
-  if (number % 3 === 0 && number % 4 != 0){ 
+  if (number % 3 === 0 && number % 4 !== 0){ 
     console.log("Loopy");
-  } else if (number % 4 === 0 && number % 3 != 0){
+  } else if (number % 4 === 0 && number % 3 !== 0){
     console.log("Lighthouse");
   } else if (number % 4 === 0 && number % 3 === 0){
     console.log("LoopyLighthouse");
