7cd792663bbf5f6e6bd53503ae033ade5c262219
sachieko <kirazetsubou@gmail.com>
2022-07-06 21:25:57 -0600

    Challenge to print Loopy for multiples of 3, Lighthouse for multiples of 4, and LoopyLighthouse for multiples of both.

---
+++ loopychallenge.js
@@ -0,0 +1,12 @@
+for (let number = 100; number <= 200; number++) {
+  // Starts running at 100, and stops inclusively at 200.
+  if (number % 3 === 0 && number % 4 != 0){ 
+    console.log("Loopy");
+  } else if (number % 4 === 0 && number % 3 != 0){
+    console.log("Lighthouse");
+  } else if (number % 4 === 0 && number % 3 === 0){
+    console.log("LoopyLighthouse");
+  } else {
+    console.log(number);
+  }
+}
