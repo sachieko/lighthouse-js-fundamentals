e9f95b93a5113a699ea50a65a1cbbf29b3ea76bd
sachieko <kirazetsubou@gmail.com>
2022-07-06 23:36:39 -0600

    Committing examples of javascript fundamentals for: an age calculator, finding particular stations in an array, printing positional coordinates from an array of directions, and iterating over arrays with while loops using a packing list example.

---
+++ chooseStations.js
@@ -0,0 +1,18 @@
+const stations = [
+  ['Big Bear Donair', 10, 'restaurant'],
+  ['Bright Lights Elementary', 50, 'school'],
+  ['Moose Mountain Community Centre', 45, 'community centre']
+];
+function chooseStations(stations){
+  let stationArray = [];
+  for (let [name, capacity, type] of stations){
+    if (capacity >= 20 && type === 'school'){
+      stationArray.push(name);
+    } else if (capacity >= 20 && type === 'community centre'){
+      stationArray.push(name);
+    }
+  }
+  return stationArray;
+}
+
+console.log(chooseStations(stations));
