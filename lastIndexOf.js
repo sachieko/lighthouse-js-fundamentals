6ca59a5c16f49b208e6901004a04d29d2ee4c093
sachieko <kirazetsubou@gmail.com>
2022-07-07 01:49:05 -0600

    Adding example functions for finding the index of the last time an element occurs and creating a range of numbers in an array with a determined step between each value.

---
+++ lastIndexOf.js
@@ -0,0 +1,14 @@
+function lastIndexOf(elements, value) {
+  let index = -1;
+  for (let i = 0; i < elements.length; i++) {
+    if (elements[i] === value) {
+      index = i;
+    }
+  } 
+  return index;
+}
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
---
+++ range.js
@@ -0,0 +1,21 @@
+function range(start, end, step){
+  if (step <= 0){
+    return [];
+  } else if (start > end){
+    return [];
+  } else if (start === null || end === null || step === null){
+    return [];
+  } else {
+    let rangearray = [];
+    let number = start;
+    for (let i = 0; i <= Math.floor((end - start) / step); i++){ 
+      rangearray.push(number);
+      number = number + step;
+    }
+    return rangearray;
+  }
+}
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
+console.log(range(5,2,3));
