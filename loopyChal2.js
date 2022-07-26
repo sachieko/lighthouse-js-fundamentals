56ab15d30c1b870d2173d0338a732a2ec33457af
sachieko <kirazetsubou@gmail.com>
2022-07-25 19:22:36 -0600

    Refactoring loopyChall2.js without else in loops

--- loopyChal2.js
+++ loopyChal2.js
@@ -3,19 +3,20 @@ const loopyLighthouse = function(range, multiple, words) {
   // multiple is an array of [multiple1, multiple2] to determine which numbers are replaced
   // words is an array of [word1, word2] to replace multiple1 and multiple2 respectively
   for (let number = range[0]; number <= range[1]; number++) {
-    // Defining conditions to save space.
-    let cond1 = number % multiple[0] === 0;
-    let cond2 = number % multiple[1] === 0;
-    // Starts running at range[0], and stops inclusively at range[i].
-    if (cond1 && !cond2) {
-      console.log(words[0]);
-    } else if (cond2 && !cond1) {
-      console.log(words[1]);
-    } else if (cond1 && cond2) {
-      console.log(words[0] + words[1]);
-    } else {
-      console.log(number);
+    // Defining conditions and empty string
+    let cond0 = number % multiple[0] === 0;
+    let cond1 = number % multiple[1] === 0;
+    let fin = '';
+    if (cond0) {
+      fin += words[0];
     }
+    if (cond1) {
+      fin += words[1];
+    }
+    if (fin === '') {
+      fin += number;
+    }
+    console.log(fin);
   }
 };
 loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
