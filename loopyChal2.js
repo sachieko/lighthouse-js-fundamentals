57b7fe6c76a299cf43415892dc9fb202d07ab6d3
sachieko <kirazetsubou@gmail.com>
2022-07-25 18:33:32 -0600

    refactoring loopyChal2.js

--- loopyChal2.js
+++ loopyChal2.js
@@ -3,12 +3,15 @@ const loopyLighthouse = function(range, multiple, words) {
   // multiple is an array of [multiple1, multiple2] to determine which numbers are replaced
   // words is an array of [word1, word2] to replace multiple1 and multiple2 respectively
   for (let number = range[0]; number <= range[1]; number++) {
+    // Defining conditions to save space. 
+    let cond1 = number % multiple[0] === 0;
+    let cond2 = number % multiple[1] === 0;
     // Starts running at range[0], and stops inclusively at range[i].
-    if (number % multiple[0] === 0 && number % multiple[1] !== 0) {
+    if (cond1 && !cond2) {
       console.log(words[0]);
-    } else if (number % multiple[1] === 0 && number % multiple[0] !== 0) {
+    } else if (cond2 && !cond1) {
       console.log(words[1]);
-    } else if (number % multiple[0] === 0 && number % multiple[1] === 0) {
+    } else if (cond1 && cond2) {
       console.log(words[0] + words[1]);
     } else {
       console.log(number);
