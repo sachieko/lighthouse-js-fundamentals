f339af6ebc859cc3499a92ff29558170e07bc997
sachieko <kirazetsubou@gmail.com>
2022-07-25 18:19:34 -0600

    Added queenThreat.js and added comments to loopyChal2.js

--- loopyChal2.js
+++ loopyChal2.js
@@ -1,15 +1,18 @@
-function loopyLighthouse(range, multiple, words) {
+const loopyLighthouse = function(range, multiple, words) {
+  // range is an array of [start,stop]
+  // multiple is an array of [multiple1, multiple2] to determine which numbers are replaced
+  // words is an array of [word1, word2] to replace multiple1 and multiple2 respectively
   for (let number = range[0]; number <= range[1]; number++) {
-    // Starts running at 100, and stops inclusively at 200.
-    if (number % multiple[0] === 0 && number % multiple[1] !== 0){ 
+    // Starts running at range[0], and stops inclusively at range[i].
+    if (number % multiple[0] === 0 && number % multiple[1] !== 0) {
       console.log(words[0]);
-    } else if (number % multiple[1] === 0 && number % multiple[0] !== 0){
+    } else if (number % multiple[1] === 0 && number % multiple[0] !== 0) {
       console.log(words[1]);
-    } else if (number % multiple[0] === 0 && number % multiple[1] === 0){
+    } else if (number % multiple[0] === 0 && number % multiple[1] === 0) {
       console.log(words[0] + words[1]);
     } else {
       console.log(number);
     }
   }
-}
+};
 loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
---
+++ queenThreat.js
@@ -0,0 +1,83 @@
+const generateBoard = (whiteQ, blackQ) => {
+  let board = [];
+  for (let i = 0; i < 8; i++) {
+    board.push([0, 0, 0, 0, 0, 0, 0, 0]);
+  }
+  board[whiteQ[0]][whiteQ[1]] = 1;
+  board[blackQ[0]][blackQ[1]] = 1;
+  return board;
+}
+const queenThreat = (board) => {
+  let bool = false;
+  //Rows
+  let rowSum = 0;
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      rowSum += board[i][j]
+    }
+    if (rowSum > 1) {
+      bool = true;
+    }
+    rowSum = 0;
+  }
+  //Columns
+  let colSum = 0;
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      colSum += board[j][i]
+    }
+    if (colSum > 1) {
+      bool = true;
+    }
+    colSum = 0;
+  }
+  //Diagonals - each double nested for loop does 1 half of the board, checking diagonals in both directions.
+  let diaSum = 0;
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0, x = i; j <= i && x >= 0; j++, x--) {
+      diaSum += board[x][j];
+    }
+    if (diaSum > 1) {
+      bool = true;
+    }
+    diaSum = 0;
+  }
+  for (let i = 7; i >= 0; i--) {
+    for (let j = 0; i + j <= 7; j++) {
+      diaSum += board[7 - j][i + j];
+    }
+    if (diaSum > 1) {
+      bool = true;
+    }
+    diaSum = 0;
+  }
+  for (let i = 7; i >= 0; i--) {
+    for (let j = 0; i + j <= 7; j++) {
+      diaSum += board[i + j][j];
+    }
+    if (diaSum > 1) {
+      bool = true;
+    }
+    diaSum = 0;
+  }
+  for (let i = 7; i >= 0; i--) {
+    for (let j = 0; i + j <= 7; j++) {
+      diaSum += board[j][i +j];
+    }
+    if (diaSum > 1) {
+      bool = true;
+    }
+    diaSum = 0;
+  }
+  return bool;
+}
+let whiteQueen = [0, 0];
+let blackQueen = [5, 7];
+let generatedBoard = generateBoard(whiteQueen, blackQueen);
+console.log(generatedBoard);
+console.log(queenThreat(generatedBoard));
+console.log(queenThreat(generateBoard([0, 0], [7, 7])));
+console.log(queenThreat(generateBoard([3, 3], [4, 4])));
+console.log(queenThreat(generateBoard([0, 6], [4, 2])));
+
+
